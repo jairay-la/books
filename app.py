eab6f48a60b3a58fa24f12239ed8b2390391f786
jairay-la <rayl.mail@gmail.com>
2025-02-10 15:26:04 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ class BookRequest(BaseModel):
 
 # Assuming Flowise is deployed and accessible at this URL
 # Replace with the actual URL of your deployed Flowise instance
-FLOWISE_API_URL = os.environ.get('FLOWISE_API_URL', 'http://https://github.com/jairay-la/books/api/v1/prediction/c8f7e207-85cb-4fed-8829-f341ce75b72e')
+FLOWISE_API_URL = os.environ.get('lAZFo7LaeVXqo_v0bSXhBKOcvIsRkVobqKY1OtkFUHc', 'http://https://github.com/jairay-la/books/api/v1/prediction/c8f7e207-85cb-4fed-8829-f341ce75b72e')
 
 @app.post("/api/process")
 async def process(book: BookRequest):
